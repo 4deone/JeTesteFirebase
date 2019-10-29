ca68398fba8ff31261be54a70fb6d8eb89ea8d73
Patrick NKOA <ndjanda@gmail.com>
2019-10-29 15:18:04 +0100

    Initialisation des éléments de la vue de l'interface de connexion

--- app/src/main/java/cm/deone/jetestefirebase/MainActivity.java
+++ app/src/main/java/cm/deone/jetestefirebase/MainActivity.java
@@ -2,12 +2,34 @@ package cm.deone.jetestefirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mRegistration;
+    Button mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRegistration = (Button)findViewById(R.id.btn_register);
+        mLogin = (Button)findViewById(R.id.btn_login);
+
+        mRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
